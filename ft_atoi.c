419c5cf28657331a543d4fc5a0133123b18b7f87
marcofs <marcofs@adot.ferreiras.marcoa@gmail.com>
2022-09-23 18:06:19 +0200

    do_tester_long_added

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,29 @@
 /*   By: marferre <marferre@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 12:34:15 by marferre          #+#    #+#             */
-/*   Updated: 2022/09/13 13:26:24 by marferre         ###   ########.fr       */
+/*   Updated: 2022/09/23 18:01:59 by marferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static unsigned long long int	ft_check_long(int x, unsigned long long int y)
+{
+	unsigned long long int	lngn;
+
+	lngn = 9223372036854775806;
+	if (y > lngn && x > 0)
+		return (-1);
+	if (y > lngn + 1 && x < 0)
+		return (0);
+	return (y);
+}
+
 int	ft_atoi(const char *str)
 {
-	int	c;
-	int	x;
-	int	y;
+	int						c;
+	int						x;
+	unsigned long long int	y;
 
 	c = 0;
 	x = 1;
@@ -34,5 +46,6 @@ int	ft_atoi(const char *str)
 		y = (str[c] - '0') + (y * 10);
 		c++;
 	}
+	ft_check_long(x, y);
 	return (x * y);
 }
