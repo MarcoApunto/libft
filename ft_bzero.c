349550575aea6ca2a671089a6b21fe41b3070cd5
Marco Antonio Ferreira Santos <marferre@c2r4s2.42madrid.com>
2022-09-26 12:52:27 +0200

    checked

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: marferre <marferre@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 13:28:02 by marferre          #+#    #+#             */
-/*   Updated: 2022/09/20 23:33:37 by marferre         ###   ########.fr       */
+/*   Updated: 2022/09/26 12:47:58 by marferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ void	ft_bzero(void *str, size_t len)
 {
 	size_t	i;
 
-	i = -1;
-	while (++i < len)
+	i = 0;
+	while (i < len)
+	{
 		((unsigned char *)str)[i] = 0;
+		i++;
+	}
 }
